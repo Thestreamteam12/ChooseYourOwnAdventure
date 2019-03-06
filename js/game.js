f58d2bd5a7ba4ddd6e304440123b04bee8eec6aa
Raynelle <raynelle.k.forrester@student.uncommoncollegiate.org>
2019-03-06 22:56:57 +0000

    ok

--- js/game.js
+++ js/game.js
@@ -2,38 +2,43 @@
 
 var game = {
     music: "98_Lost_Mine.mp3",
-    background_image: "intro-bg.jpg",
+    background_image: "ghost.jpg",
     levels: {
 
         start: {
-            message: "You come across a dark cave",
+            message: "You find a dead body and with it there is gold",
             choices: [
                 {
-                    text: "Enter the cave",
-                    nextLevel: "cave",
+                    text: "Take the gold",
+                    nextLevel: "body",
                 },
 
                 {
-                    text: "Keep on moving",
+                    text: "call the cops",
                     nextLevel: "field",
                 },
             ]
         },
 
-        cave: {
-            background_image: "fire.gif",
-            music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "You come across a fire monster or something!",
+        body: {
+            background_image: "ghost2.jpeg",
+            music: "98_Lost_Mine.mp3",
+            message: "You're a horrible person, however, a ghost appears",
             choices: [
                 {
-                    text: "Start over",
-                    nextLevel: "start",
+                    text: "You talk to the ghost",
+                    nextLevel: "start2",
                 },
+                
+                {
+                    text: "You run away screaming",
+                    nextLevel: "start1"
+                }
             ]
         },
 
         field: {
-            message: "Some adventurer you are...",
+            message: "The cops come and accuse you of murder",
             choices: [
                 {
                     text: "Start over",
@@ -41,6 +46,47 @@ var game = {
                 },
             ]
         },
-
+    start1: {
+        message: "You trip over a rock, hit your head, and die",
+        choices: [
+                {
+                    text: "Start over",
+                    nextLevel: "start",
+                }
+            ]
+    },
+    start2: {
+        message: "Ghost tells you to murder the man who murdered him...it's his cousin, he wants you to murder him!",
+        choices: [
+                { 
+                    text: "You leave the ghost and take his gold and the ghost is sad",
+                    nextLevel: "start4",
+                },
+                
+                {
+                    text: "You accept the quest",
+                    nextLevel: "start3",
+                    
+                }
+            ]
+    },
+        start3: {
+            message: "To be continued...",
+            choices: [
+                {
+                    text: "Start over",
+                    nextLevel: "start",
+                }
+                ]
+    },
+        start4: {
+            message: "You'll never be a TRUE hero",
+            choices: [
+                {
+                text: "Start over",
+                nextLevel: "start",
+                }
+                ]
+        }
     }
-};
+}
